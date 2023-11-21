8fffe17b92b591d313284f6c80fc690f98cf19df
Carlos Dória <carlosdoria953@gmail.com>
2023-11-20 21:35:00 -0300

    test: ensure RemoteAuthentication throws InvalidCredentialsError on 401

--- src/data/tests/mock-http-client.ts
+++ src/data/tests/mock-http-client.ts
@@ -1,16 +1,23 @@
 import {
-  type HttpPostClientParams,
   type HttpPostClient,
+  type HttpPostClientParams,
 } from "@/data/protocols/http/http-post-client";
+import {
+  type HttpResponse,
+  HttpStatusCode,
+} from "@/data/protocols/http/http-response";
 
 export class HttpPostClientSpy implements HttpPostClient {
   url?: string;
   body?: object;
+  response: HttpResponse = {
+    statusCode: HttpStatusCode.noContent,
+  };
 
-  async post(params: HttpPostClientParams): Promise<void> {
+  async post(params: HttpPostClientParams): Promise<HttpResponse> {
     this.url = params.url;
     this.body = params.body;
 
-    await Promise.resolve();
+    return await Promise.resolve(this.response);
   }
 }
--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,5 +1,7 @@
+import { HttpStatusCode } from "@/data/protocols/http/http-response";
 import { RemoteAuthentication } from "./remote-authentication";
 import { HttpPostClientSpy } from "@/data/tests/mock-http-client";
+import { InvalidCredentialsError } from "@/domain/errors/invalid-credential-error";
 import { mockAuthentication } from "@/domain/test/mock-authentication";
 import faker from "faker";
 
@@ -35,5 +37,17 @@ describe("RemoteAuthentication", () => {
     await sut.auth(authenticationParams);
 
     expect(httpPostClientSpy.body).toEqual(authenticationParams);
+  });
+
+  test("Should throw InvalidCredentialsError if HttpPostClient return 401", async () => {
+    const { sut, httpPostClientSpy } = makeSut();
+
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.unauthorized,
+    };
+
+    const promise = sut.auth(mockAuthentication());
+
+    await expect(promise).rejects.toThrow(new InvalidCredentialsError());
   });
 });
